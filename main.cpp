bcc465a6628413233183f807d32f36980e08efe4
ZyneAA <htet.zyne.aa@gmail.com>
2023-10-04 20:19:02 +0630

    brach test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 // Test
 int main(int argc, char* argv[]){
+
+    if(argc < 2){
+        std::cout << "Need the source file." << std::endl;
+    }
     Lexer l1(argv[1]);
     l1.make_vector();
     Token t1(l1.TT);
